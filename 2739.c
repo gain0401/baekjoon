29f436bc7ee0a5846049513222132b5d758d3359
gain0401 <gainlee2004@gmail.com>
2025-04-08 21:04:37 +0900

    Create 2739.c
    Times table

---
+++ 2739.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+	int n, i = 1, result;
+
+	scanf("%d", &n);
+
+	if (1 <= n <= 10) {
+		while (i <= 9) {
+			result = n * i;
+			printf("%d * %d = %d \n", n, i, result);
+			i++;
+		}
+	}
+	else
+		return 0;
+}
