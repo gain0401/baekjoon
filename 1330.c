e51120777103f872d8b0ecd06a258e97d9eb097c
gain0401 <gainlee2004@gmail.com>
2025-04-08 20:39:54 +0900

    Create 1330.c
    Compare A whit B

---
+++ 1330.c
@@ -0,0 +1,21 @@
+int main() {
+	
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+
+	if (-10000 <= a, b <= 10000) {
+		if (a > b) {
+			printf(">"); 
+		}
+		else if (a == b) {
+			printf("==");
+		}
+		else {
+			printf("<");
+		}
+	}
+		else
+			return 0;
+
+}
