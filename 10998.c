fe4eb8e2391e4731bb6e85bf017ef1d7f6f1976a
gain0401 <gainlee2004@gmail.com>
2025-07-02 17:15:57 +0900

    Baekjoon Number-10998.c
     A x B = ?

---
+++ 10998.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int A, B;
+    
+    scanf("%d %d", &A, &B);
+    
+    if (A > 0) {
+        if (B < 10) {
+           printf("%d\n", A*B);         
+        }
+    }
+   
+    return 0;
+}
