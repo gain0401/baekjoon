750dce708c64b5233da4356b07b57c08b6b33e1f
gain0401 <gainlee2004@gmail.com>
2025-07-07 16:47:56 +0900

    Baekjoon Number-10869.c
    A + B
    
    A - B
    
    A * B
    
    A / B 
    
    A % B

---
+++ 10869.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int A, B;
+    
+    scanf("%d %d", &A, &B);
+
+    
+    if (A < 1 || B > 10000) 
+        return 0;
+
+    printf("%d\n", A+B);
+    printf("%d\n", A-B);
+    printf("%d\n", A*B);
+    printf("%d\n", A/B);
+    printf("%d\n", A%B);
+    
+   
+    return 0;
+}
